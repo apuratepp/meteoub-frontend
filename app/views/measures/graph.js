14c29715ae501e3e5e30b1045e8c5e83b703e06f
Josep <apuratepp@gmail.com>
2014-07-06 19:06:28 +0200

    fix d3 graph color + nav bar as component

--- app/views/measures/graph.js
+++ app/views/measures/graph.js
@@ -2,9 +2,15 @@ export default Ember.View.extend({
 	didInsertElement: function() {
 		this.loadGraph();
 	},
+	willDestroyElement: function() {
+		this.destroyGraph();
+	},
+	destroyGraph: function() {
+		d3.select("svg").remove();
+	},
 	loadGraph: function() {
-		var width = 960;
-		var height = 480;
+		var width = $(window).width();
+		var height = $(window).height();
 		
 		var x = d3.scale.linear().range([0, width]);
 		var y = d3.scale.ordinal().rangeRoundBands([0, height], 0.2);
@@ -17,28 +23,38 @@ export default Ember.View.extend({
 								.append('g');
 
 		d3.json('http://meteoub.equip9.org/measures', function(error, data) {
+
 			x.domain(d3.extent(data.measures, function(measure){
 				return measure.temperature;
 			}));
+			
 			y.domain(data.measures.map(function(measure) {
 				return measure.datetime;
 			}));
 
-		svg.selectAll(".bar").data(data.measures).enter().append("rect")
-					.attr("class", function(measure) { return measure.temperature < 0 ? "bar negative" : "bar positive"; })
-					.attr("x", function(measure) { 
-						// return x(Math.min(0, measure.temperature)); 
-						return 10;
-					})
-					.attr("y", function(measure) { return y(measure.datetime); })
-					.attr("width", function(measure) { 
-						// return Math.abs(x(measure.temperateure) - x(0)); 
-						return 100;
-					})
-					.attr("height", y.rangeBand());
+			svg.selectAll(".bar").data(data.measures).enter().append("rect")
+						.attr("class", function(measure) { 
+							return measure.temperature < 0 ? "bar negative" : "bar positive"; 
+						})
+						.attr("x", function(measure) { 
+							return width / 10.0;
+						})
+						.attr("y", function(measure) { 
+							return y(measure.datetime); 
+						})
+						.attr("width", function(measure) { 
+							var relative_temperature = measure.temperature / 40.0; // relative to 0º C-40º C
 
-		svg.append("g").attr("class", "x axis").call(xAxis);
+							return width * relative_temperature;
+						})
+						.attr("height", y.rangeBand())
+						.attr("fill", function(measure) {
+							var relative_temperature = measure.temperature / 40.0; // relative to 0º C-40º C
+							var color = 'orange';
+							return color;
+						});
 
+			svg.append("g").attr("class", "x axis").call(xAxis);
 			svg.append("g").attr("class", "y axis").append("line")
 					.attr("x1", x(0))
 					.attr("x2", x(0))
